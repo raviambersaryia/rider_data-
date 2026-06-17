7e7c28b483bf5e782fadaa8ed8d0e99015035325
raviambersaryia <jattpartik8@gmail.com>
2026-06-17 18:28:38 +0530

    Update script.js
    edit and deleted button are removed

--- static/script.js
+++ static/script.js
@@ -910,14 +910,8 @@ function createRiderCard(rider) {
                 </div>
             </div>
 
-            // <div class="rider-actions">
-            //     <button class="rider-action-btn rider-edit-btn" onclick="editRider('${rider.id}')">
-            //         ✏️ Edit
-            //     </button>
-            //     <button class="rider-action-btn rider-delete-btn" onclick="openDeleteConfirmation('${rider.id}')">
-            //         🗑️ Delete
-            //     </button>
-            // </div>
+           
+             </div>
         </div>
     `;
 
